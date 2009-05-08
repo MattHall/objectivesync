15b3d37bedd583d1e1e6342465a63e0d02e03d85
Justin Cunningham <justin@justin-cunninghams-computer.local>
2009-05-07 20:30:29 -0400

    Added some inline docs about nested model usage

--- NotesExampleApp/Note.m
+++ NotesExampleApp/Note.m
@@ -30,11 +30,33 @@
 
 - (SQLitePersistentObject *)parent {
 	return [Person findFirstByCriteria:[NSString stringWithFormat:@"WHERE person_id = '%@'", self.personId]];
+	/*
+	 this method is used by the sync service to determine what parent information should be saved in the
+	 event log so that the record can be deleted if it is nested.  In the event you have a polymorphic design,
+	 where the object could potentially have any number of parents, you'll have to add your own logic here
+	 so that the sync service will know which parent to use.
+	 
+	 E.G. In order to delete the note with ID 1, that belongs to parent 4, a delete request would have to be
+	 sent to http://host/people/4/notes/1.xml.  Since the object is destroyed locally and then logged,
+	 we need to make the parent available to the sync service so that it can track the parentId or 4,
+	 along with the remoteId of the record.
+	 
+	 This method should only be implemented if the model is nested.
+	 */
 }
 
 - (NSString *)remoteFindBase {
 	return [NSString stringWithFormat:@"%@/%@",self.personId,[Note getRemoteCollectionName]];	
 }
+
+/*
+ You'll want to override the below methods for nested models so that create, update, and delete are
+ handled correctly based on your nesting model.  This allows the sync service to be relatively clueless
+ about how you have things laid out on the server side.
+ 
+ For delete to function, the above parent method must be set as well, so the parent ID and class name
+ will be logged after the record is destroyed locally.
+ */
 
 - (BOOL)createRemoteWithResponse:(NSError **)aError {
 	return [self createRemoteAtPath:[Person getRemoteElementPath:[self nestedPath]] withResponse:aError];
